10a65de3c6656a18dd073e8480325f03b3ad7366
360techhunts <360techhunts@gmail.com>
2026-04-30 12:46:10 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'finance-pro-cache-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+// Install the service worker and cache the core files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Serve cached files for fast loading
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version if found, else fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Update the cache when a new version of the app is released
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
